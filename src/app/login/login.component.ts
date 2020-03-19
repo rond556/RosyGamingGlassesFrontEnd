bba5a10cbede98c9f00db8b5d1b24ba16d523b69
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-03-19 10:32:18 -0400

    refactored

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
 import { Router } from  '@angular/router';
 import { AuthService } from '../services/auth.service';
-
+import { LoginScreenGifs } from 'src/models/loginscreengifs';
 
 
 
@@ -14,53 +14,14 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   isSubmitted  =  false;
-  images: string[] = ['https://media.giphy.com/media/AuIu79zI80VVe/giphy.gif',
-  'https://media.giphy.com/media/BrV5mGUrn5Li/giphy.gif',
-  'https://media.giphy.com/media/kCug4YeBhzxrq/giphy.gif',
-  'https://media.giphy.com/media/10H2lNJsW1lOHC/giphy.gif',
-  'https://media.giphy.com/media/yiQFNwgSBg7As/giphy.gif',
-  'https://media.giphy.com/media/TSKCDXGSBDJks/giphy.gif',
-  'https://media.giphy.com/media/RRef9mlH6YiKQ/giphy.gif',
-  'https://media.giphy.com/media/BS5dtZMwxWRTq/giphy.gif',
-  'https://media.giphy.com/media/AO5qaphTxRnyw/giphy.gif',
-  'https://media.giphy.com/media/1ZuJaGQR7sGc9zL8Hh/giphy.gif',
-  'https://media.giphy.com/media/zF9gqOiTb95bW/giphy.gif',
-  'https://media.giphy.com/media/xRNpHfgk9OeUU/giphy.gif',
-  'https://media.giphy.com/media/qzDJ06BW5OQCY/giphy.gif',
-  'https://media.giphy.com/media/jFYBRC7NzGrNS/giphy.gif',
-  'https://media.giphy.com/media/eRfpAFGHAfcXu/giphy.gif',
-  'https://media.giphy.com/media/REgFJcnd2n21O/giphy.gif',
-  'https://media.giphy.com/media/DpXqHdILXRRDi/giphy.gif',
-  'https://media.giphy.com/media/i5dDJz6oQHFD2/giphy.gif',
-  'https://media.giphy.com/media/h7U5kvQ7Lwzok/giphy.gif',
-  'https://media.giphy.com/media/98STDd3SLagaQ/giphy.gif',
-  'https://media.giphy.com/media/TtWC9oazPXiOA/giphy.gif',
-  'https://media.giphy.com/media/hldSOLuYpBKus/giphy.gif',
-  'https://media.giphy.com/media/EHbUWNDChZBw4/giphy.gif',
-  'https://media.giphy.com/media/nhl3WoySrHVRK/giphy.gif',
-  'https://media.giphy.com/media/a6e24oTqeSjcs/giphy.gif',
-  'https://media.giphy.com/media/D1iDxmIGxUnzq/giphy.gif',
-  'https://media.giphy.com/media/vmZ11AetEXR6M/giphy.gif',
-  'https://media.giphy.com/media/1dTuqX7kVCwdW/giphy.gif',
-  'https://media.giphy.com/media/5yUO10BpJ5JLy/giphy.gif',
-  'https://media.giphy.com/media/jplF48QI3L3Ec/giphy.gif',
-  'https://media.giphy.com/media/xUNd9IjAp7XhuwHRao/giphy.gif',
-  'https://media.giphy.com/media/txcIHRNl2vcDm/giphy.gif',
-  'https://media.giphy.com/media/asRx7MPFS0SBO/giphy.gif',
-  'https://media.giphy.com/media/6UDorPg1oGRQ4/giphy.gif',
-  'https://media.giphy.com/media/yiBt9PZKrwg0g/giphy.gif',
-  'https://media.giphy.com/media/jneiA7ImMXF6/giphy.gif',
-  'https://media.giphy.com/media/3o6EQaAxwqYByiZXIQ/giphy.gif',
-  'https://media.giphy.com/media/1vZ6QcjgF2iARTj4BE/giphy.gif',
-'https://media.giphy.com/media/pcKpMur7aAYMCs7WQX/giphy.gif',
-'https://media.giphy.com/media/f4JcszAA6d0c39QeUC/giphy.gif',
-'https://media.giphy.com/media/slvREYowfwgo0/giphy.gif',
-'https://media.giphy.com/media/CYFzxzmE2oDkI/giphy.gif',
-'https://media.giphy.com/media/l11R93B7B4CCA/giphy.gif'];
+  images;
 
   currentBackground = this.images[Math.floor(Math.random() * this.images.length)];
 
-  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder ) { }
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder, private loginGifs: LoginScreenGifs ) {
+   this.images = loginGifs.getGifArray;
+   }
+
 
 
   ngOnInit() {
@@ -85,6 +46,6 @@ login(){
 }
 
 changeImage() {
-  this.currentBackground = this.images[Math.floor(Math.random() * this.images.length)];
+  this.currentBackground = 'https://media.giphy.com/media/' + this.images[Math.floor(Math.random() * this.images.length)];
  }
 }
