dcbf89c78a4d8410b5e6ee2c84aca82fce17f206
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-03-26 14:26:54 -0400

    login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -92,6 +92,7 @@ onSubmit(){
   this.authService.login(this.loginPayload).subscribe(data => {
     if(data){
       console.log("Login Success")
+      this.router.navigateByUrl('/home');
     } else {
       console.log("Login Failed")
     }
